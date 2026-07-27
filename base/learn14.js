84832cdab807b98927aaad56983d472eda78037e
FitiavanaLova <hei.fitiavana@gmail.com>
2026-07-27 22:10:49 +0300

    fix : add city to the object

--- base/learn14.js
+++ base/learn14.js
@@ -3,8 +3,10 @@
 let etudiant = {
     nom: "John Doe",
     age: 20,
-    moyenne: 15.5
+    moyenne: 15.5,
+    ville: "Paris"
 };  
 console.log("Nom : " + etudiant.nom);
 console.log("Age : " + etudiant.age);
 console.log("Moyenne : " + etudiant.moyenne);
+console.log("Ville : " + etudiant.ville);
