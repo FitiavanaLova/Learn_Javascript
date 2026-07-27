f72fd98de89f7b217bc298068452e06ce1aeacb4
FitiavanaLova <hei.fitiavana@gmail.com>
2026-07-27 22:08:24 +0300

    feat : learn object

---
+++ base/learn14.js
@@ -0,0 +1,10 @@
+//object
+
+let etudiant = {
+    nom: "John Doe",
+    age: 20,
+    moyenne: 15.5
+};  
+console.log("Nom : " + etudiant.nom);
+console.log("Age : " + etudiant.age);
+console.log("Moyenne : " + etudiant.moyenne);
