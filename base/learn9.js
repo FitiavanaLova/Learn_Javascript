55ad807ce7cf9d3177cbbe7432e79e8fe8b7f036
FitiavanaLova <hei.fitiavana@gmail.com>
2026-07-27 21:42:02 +0300

    feat : create function estPair

---
+++ base/learn9.js
@@ -0,0 +1,10 @@
+//Crée une fonction estPair(nombre)qui retourne :true ou false
+function estPair(nombre){
+    if(nombre%2==0){
+        return true;
+    } else{
+        return false;
+    }
+}
+console.log(estPair(4));
+console.log(estPair(5));        
